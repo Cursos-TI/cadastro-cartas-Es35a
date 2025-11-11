80e63dd5b21f1e5d879e18c6b9e38001f9767146
SANIA ESTHER DE SOUSA COSTA <sanya-esther@hotmail.com>
2025-11-11 01:55:08 +0000

    trabalho carta 1

---
+++ supertrunfo/supertrunfo.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+int main(){
+    int Carta;
+    char Estado[30];
+    char Codigo[30];
+    char Nome_da_cidade[50];
+    int Populacao;
+    float Area_em_km;
+    float Pib;
+    int Numero_de_pontos_turisticos;
+    
+    
+
+
+    
+
+}
